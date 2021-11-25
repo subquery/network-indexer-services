19959ff2ab96089c074540a1278337fda590695a
Scott Twiname <skott.twiname@gmail.com>
2021-11-26 10:43:27 +1300

    Move cli to yargs

--- src/cli/cli.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BasicCommand } from './cli.command';
-
-@Module({
-  providers: [BasicCommand],
-  exports: [BasicCommand]
-})
-export class CliModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app.module';
 import { getLogger, LogCategory, NestLogger } from './utils/logger';
-import { CliModule } from './cli/cli.module';
+import { argv } from './yargs';
 
 async function bootstrap() {
   try {
-    // TODO: fix cli issue
-    await CommandFactory.run(CliModule);
+    const port = argv('port') as number;
     const app = await NestFactory.create(AppModule, { logger: new NestLogger() });
-    await app.listen(3001);
+    await app.listen(port);
     getLogger(LogCategory).info('coordinator service started');
   } catch (e) {
     getLogger(LogCategory).error(e, 'coordinator service failed');
