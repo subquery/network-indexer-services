ca64e60d85573c562689bee9ec0aa8c0f28959fc
Neo Sun <sun@subquery.network>
2022-09-27 16:57:42 +1300

    Add labor log table and db functions

---
+++ tasks/src/models.rs
@@ -0,0 +1,153 @@
+use ethers::types::{Address, Bytes, U256};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::env;
+use tokio::sync::OnceCell;
+
+pub static DB: OnceCell<PgPool> = OnceCell::const_new();
+
+pub fn db<'a>() -> &'a PgPool {
+    DB.get().expect("Database lost connections")
+}
+
+pub async fn setup_db() {
+    // setup database connection
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("Database connection failed");
+    DB.set(db).expect("Database connection failed");
+}
+
+pub async fn channel_checkpoint(id: U256, onchain: U256) {
+    let id_string = format!("{:#X}", id);
+    let onchain_string = format!("{}", onchain);
+    let _ = query!(
+        "UPDATE channel SET onchain=$1 WHERE id=$2",
+        onchain_string,
+        id_string
+    )
+    .execute(db())
+    .await;
+}
+
+pub async fn channel_extend(id: U256, expiration: U256) {
+    let id_string = format!("{:#X}", id);
+    let expiration_int = expiration.as_u64() as i32;
+    let _ = query!(
+        r#"UPDATE channel SET "expirationAt"=$1 WHERE id=$2"#,
+        expiration_int,
+        id_string
+    )
+    .execute(db())
+    .await;
+}
+
+pub async fn channel_fund(id: U256, total: U256) {
+    let id_string = format!("{:#X}", id);
+    let total_string = format!("{}", total);
+    let _ = query!(
+        "UPDATE channel SET total=$1 WHERE id=$2",
+        total_string,
+        id_string
+    )
+    .execute(db())
+    .await;
+}
+
+pub async fn channel_challenge(id: U256, onchain: U256) {
+    let id_string = format!("{:#X}", id);
+    let onchain_string = format!("{}", onchain);
+    let _ = query!(
+        "UPDATE channel SET onchain=$1,status=$2 WHERE id=$3",
+        onchain_string,
+        2, // ChannelStatus::CHALLENGE
+        id_string
+    )
+    .execute(db())
+    .await;
+}
+
+pub async fn channel_respond(id: U256, onchain: U256) {
+    let id_string = format!("{:#X}", id);
+    let onchain_string = format!("{}", onchain);
+    let _ = query!(
+        "UPDATE channel SET onchain=$1,status=$2 WHERE id=$3",
+        onchain_string,
+        1, // ChannelStatus::OPEN
+        id_string
+    )
+    .execute(db())
+    .await;
+}
+
+pub async fn channel_finalize(id: U256, total: U256, remain: U256) {
+    let id_string = format!("{:#X}", id);
+    let onchain_string = format!("{}", total - remain);
+    let _ = query!(
+        "UPDATE channel SET onchain=$1,status=$2 WHERE id=$3",
+        onchain_string,
+        0, // ChannelStatus::FINALIZE
+        id_string
+    )
+    .execute(db())
+    .await;
+}
+
+pub async fn channel_labor(deployment_id: Bytes, indexer: Address, amount: U256, time: u64) {
+    let deployment_string = deployment_cid(&deployment_id);
+    let indexer_string = format!("{:?}", indexer);
+    let total_string = format!("{}", amount);
+    let _ = query!(
+        r#"
+          INSERT INTO channel_labor("deploymentId",indexer,total,"createdAt")
+          VALUES ($1,$2,$3,$4)
+        "#,
+        deployment_string,
+        indexer_string,
+        total_string,
+        time as i32
+    )
+    .execute(db())
+    .await;
+}
+
+pub async fn init_chain_block() -> u64 {
+    let res = query!("SELECT value FROM chain_info WHERE name = 'block'")
+        .fetch_one(db())
+        .await;
+
+    if let Ok(res) = res {
+        res.value.parse::<u64>().unwrap()
+    } else {
+        let _ = query!("INSERT INTO chain_info(name,value) VALUES ('0', 'block')")
+            .execute(db())
+            .await;
+        0
+    }
+}
+
+pub async fn update_chain_block(block: u64) {
+    let _ = query!(
+        "UPDATE chain_info SET value=$1 WHERE name = 'block'",
+        format!("{}", block)
+    )
+    .execute(db())
+    .await;
+}
+
+fn deployment_cid(deployment: &[u8]) -> String {
+    if deployment.len() != 32 {
+        return "".to_owned();
+    }
+    // Add our default ipfs values for first 2 bytes:
+    // function:0x12=sha2, size:0x20=256 bits
+    // and cut off leading "0x"
+    let mut bytes = [0u8; 34];
+    bytes[0] = 18;
+    bytes[1] = 32;
+    bytes[2..].copy_from_slice(deployment);
+    bs58::encode(&bytes).into_string()
+}
--- tasks/src/state-channel.rs
+++ tasks/src/state-channel.rs
@@ -1,5 +1,10 @@
+#[macro_use]
+extern crate sqlx;
+
 use ethers::prelude::*;
 use subql_contracts::{state_channel, Network};
+
+mod models;
 
 #[allow(non_snake_case)]
 #[derive(Clone, Debug, EthEvent)]
@@ -63,19 +68,23 @@ struct ChannelFinalize {
     remain: U256,
 }
 
+#[allow(non_snake_case)]
+#[derive(Clone, Debug, EthEvent)]
+struct ChannelLabor {
+    deploymentId: Bytes,
+    indexer: Address,
+    amount: U256,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ()> {
     let client = Provider::<Ws>::connect("wss://moonbeam-alpha.api.onfinality.io/public-ws")
         .await
         .unwrap();
 
-    let last_block = client
-        .get_block(BlockNumber::Latest)
-        .await
-        .unwrap()
-        .unwrap()
-        .number
-        .unwrap();
+    models::setup_db().await;
+    let last_block = models::init_chain_block().await;
+
     println!("last_block: {}", last_block);
 
     let contract = state_channel(client, Network::Moonbase).unwrap();
@@ -101,6 +110,9 @@ async fn main() -> Result<(), ()> {
     let event_finalize = contract.event::<ChannelFinalize>().from_block(last_block);
     let mut stream_finalize = event_finalize.stream().await.unwrap().with_meta();
 
+    let event_labor = contract.event::<ChannelLabor>().from_block(last_block);
+    let mut stream_labor = event_labor.stream().await.unwrap().with_meta();
+
     loop {
         tokio::select! {
             Some(e1) = stream_open.next() => {
@@ -109,33 +121,52 @@ async fn main() -> Result<(), ()> {
                 }
             }
             Some(e2) = stream_extend.next() => {
-                if let Ok((_extend, log)) = e2 {
+                if let Ok((extend, log)) = e2 {
+                    models::channel_extend(extend.channelId, extend.expiration).await;
+                    models::update_chain_block(log.block_number.as_u64()).await;
                     println!("EXTEND at block {}", log.block_number);
                 }
             }
             Some(e3) = stream_fund.next() => {
-                if let Ok((_fund, log)) = e3 {
+                if let Ok((fund, log)) = e3 {
+                    models::channel_fund(fund.channelId, fund.total).await;
+                    models::update_chain_block(log.block_number.as_u64()).await;
                     println!("FUND at block {}", log.block_number);
                 }
             }
             Some(e4) = stream_checkpoint.next() => {
-                if let Ok((_checkpoint, log)) = e4 {
+                if let Ok((checkpoint, log)) = e4 {
+                    models::channel_checkpoint(checkpoint.channelId, checkpoint.spent).await;
+                    models::update_chain_block(log.block_number.as_u64()).await;
                     println!("CHECKPOINT at block {}", log.block_number);
                 }
             }
             Some(e5) = stream_challenge.next() => {
-                if let Ok((_challenge, log)) = e5 {
+                if let Ok((challenge, log)) = e5 {
+                    models::channel_challenge(challenge.channelId, challenge.spent).await;
+                    models::update_chain_block(log.block_number.as_u64()).await;
                     println!("CHALLENGE at block {}", log.block_number);
                 }
             }
             Some(e6) = stream_respond.next() => {
-                if let Ok((_respond, log)) = e6 {
+                if let Ok((respond, log)) = e6 {
+                    models::channel_respond(respond.channelId, respond.spent).await;
+                    models::update_chain_block(log.block_number.as_u64()).await;
                     println!("RESPOND at block {}", log.block_number);
                 }
             }
             Some(e7) = stream_finalize.next() => {
-                if let Ok((_finalize, log)) = e7 {
+                if let Ok((finalize, log)) = e7 {
+                    models::channel_finalize(finalize.channelId, finalize.total, finalize.remain).await;
+                    models::update_chain_block(log.block_number.as_u64()).await;
                     println!("FINALIZE at block {}", log.block_number);
+                }
+            }
+            Some(e8) = stream_labor.next() => {
+                if let Ok((labor, log)) = e8 {
+                    models::channel_labor(labor.deploymentId, labor.indexer, labor.amount, log.block_number.as_u64()).await;
+                    models::update_chain_block(log.block_number.as_u64()).await;
+                    println!("LABOR at block {}", log.block_number);
                 }
             }
             else => break,
