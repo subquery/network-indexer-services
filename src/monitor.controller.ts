8bfbe6a1f11ab2a8ebb6626f15e585c33d28f0da
Neo Sun <sun@subquery.network>
2022-11-16 15:29:01 +1300

    Add comments to monitor

--- src/monitor.controller.ts
+++ src/monitor.controller.ts
@@ -4,14 +4,23 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 
 class Proxy {
+  // percentage of used cpu, eg. 25, means 25%
   p_cpu: number;
+  // total memory size, unit is GB
   t_mem: number;
+  // percentage of used memory
   p_mem: number;
+  // total disk size, unit is GB
   t_disk: number;
+  // percentage of used disk
   p_disk: number;
+  // peer id in the p2p network
   peer: string;
+  // peer address in the p2p network
   addr: string;
+  // the number of actived agreements
   agreement: number;
+  // the number of actived state channels
   channel: number;
 }
 
