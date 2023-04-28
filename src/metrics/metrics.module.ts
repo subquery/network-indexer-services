38fa127008daa465e4c959c49c2af729b837a9b9
HZHAIX <zhaihaohk@gmail.com>
2023-04-28 12:08:25 +1200

    Remove old metrics and payg syncing (#83)
    * Remove old metrics and payg syncing
    
    * Increase version number

--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -5,13 +5,12 @@ import { Module } from '@nestjs/common';
 
 import { AccountModule } from 'src/account/account.module';
 import { ServicesModule } from 'src/services/services.module';
-import { PublicMetricsService } from './public.metric.service';
 import { MetricsResolver } from './metrics.resolver';
 import { VersionsService } from './versions.service';
 
 @Module({
   imports: [AccountModule, ServicesModule],
-  providers: [PublicMetricsService, VersionsService, MetricsResolver],
-  exports: [PublicMetricsService],
+  providers: [VersionsService, MetricsResolver],
+  exports: [],
 })
 export class MetricsModule {}
