ddad3b71bfc072c23c01a7d58a073db4549f4727
HZ <zhaihaohk@gmail.com>
2023-06-07 14:25:50 +1200

    Don't throw error to break the process

--- src/core/account.service.ts
+++ src/core/account.service.ts
@@ -35,7 +35,6 @@ export class AccountService {
 
   async getIndexer(): Promise<string> {
     if (!this.indexer) {
-      logger.info(`indexer registry: ${this.contractService.getSdk().indexerRegistry.address}`)
       const indexer = await this.indexerRepo.findOne();
       this.indexer = indexer?.address;
     }
