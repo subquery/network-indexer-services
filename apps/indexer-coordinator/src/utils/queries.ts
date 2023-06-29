3614dbff570a95a15c5b306be846e29bd2ed7756
cyrbuzz <cyrbuzz@foxmail.com>
2023-06-29 14:07:26 +0800

    feat: remove react and change @apollo/client to @apollo/client/core

--- apps/indexer-coordinator/src/utils/queries.ts
+++ apps/indexer-coordinator/src/utils/queries.ts
@@ -1,9 +1,10 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { gql } from '@apollo/client';
+import { gql } from '@apollo/client/core';
 import { DocumentNode } from 'graphql';
 
+// @ts-ignore
 export const GET_DEPLOYMENT: DocumentNode = gql`
   query GetDeployment($id: String!) {
     deployment(id: $id) {
@@ -20,6 +21,7 @@ export const GET_DEPLOYMENT: DocumentNode = gql`
   }
 `;
 
+// @ts-ignore
 export const GET_INDEXER_PROJECTS: DocumentNode = gql`
   query GetIndexerProjects($indexer: String!) {
     deploymentIndexers(filter: { indexerId: { equalTo: $indexer } }) {
