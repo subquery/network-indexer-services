cc88c9a7b299bf44c12aa5175daf72d60fdf70a8
mzxyz <mzxyz@gmail.com>
2023-07-11 17:32:18 +1200

    Remove hook for db migration

--- src/migration/1688676100216-addAgentPayg.ts
+++ src/migration/1688676100216-addAgentPayg.ts
@@ -7,20 +7,10 @@ export class AddAgentPayg1688676100216 implements MigrationInterface {
   name = 'AddAgentPayg1688676100216';
 
   async up(queryRunner: QueryRunner): Promise<void> {
-    const table = await queryRunner.getTable('channel');
-    const isAgentColumnExist = table.columns.find((column) => column.name === 'agent');
-
-    if (!isAgentColumnExist) {
-      await queryRunner.query(`ALTER TABLE "channel" ADD "agent" character varying NOT NULL DEFAULT ''`);
-    }
+    await queryRunner.query(`ALTER TABLE "channel" ADD "agent" character varying NOT NULL DEFAULT ''`);
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {
-    const table = await queryRunner.getTable('channel');
-    const isAgentColumnExist = table.columns.find((column) => column.name === 'agent');
-
-    if (isAgentColumnExist) {
-      await queryRunner.query(`ALTER TABLE "channel" DROP COLUMN "agent"`);
-    }
+    await queryRunner.query(`ALTER TABLE "channel" DROP COLUMN "agent"`);
   }
 }
