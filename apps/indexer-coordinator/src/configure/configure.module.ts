5294facea29032dd69d73f147e4d771ee731941b
mzxyz <mzxyz@gmail.com>
2023-07-26 14:17:16 +1200

    Remove `dev` in IConfig

--- apps/indexer-coordinator/src/configure/configure.module.ts
+++ apps/indexer-coordinator/src/configure/configure.module.ts
@@ -20,7 +20,6 @@ export interface IConfig {
   readonly port: number;
   readonly postgres: Postgres;
   readonly debug: boolean;
-  readonly dev: boolean;
   readonly secret: string;
   readonly startPort: number;
   readonly dockerNetwork: string;
@@ -41,7 +40,6 @@ export class Config implements IConfig {
       wsEndpoint: argv['ws-endpoint'],
       port: argv['port'],
       debug: argv['debug'],
-      dev: argv['dev'],
       secret: argv['secret-key'],
       startPort: argv['start-port'],
       dockerNetwork: argv['docker-network'],
