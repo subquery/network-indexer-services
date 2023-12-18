2604c6f7dde0b6b22e68190a97a5c49285605c7e
Jacob <princewith@gmail.com>
2023-12-18 11:09:52 +0800

    update contract sdk version

--- apps/indexer-coordinator/src/utils/contractSDK.ts
+++ apps/indexer-coordinator/src/utils/contractSDK.ts
@@ -1,45 +1,43 @@
 // Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { ContractSDK, SdkOptions } from '@subql/contract-sdk';
-import keplerDeployment from '@subql/contract-sdk/publish/kepler.json';
-import mainnetDeployment from '@subql/contract-sdk/publish/mainnet.json';
-import testnetDeployment from '@subql/contract-sdk/publish/testnet.json';
+import { ContractSDK, SdkOptions, SubqueryNetwork } from '@subql/contract-sdk';
+// import keplerDeployment from '@subql/contract-sdk/publish/kepler.json';
+// import mainnetDeployment from '@subql/contract-sdk/publish/mainnet.json';
+// import testnetDeployment from '@subql/contract-sdk/publish/testnet.json';
 import { Signer, providers } from 'ethers';
 
-const deployments = {
-  testnet: testnetDeployment,
-  kepler: keplerDeployment,
-  mainnet: mainnetDeployment,
-};
+// const deployments = {
+//   testnet: testnetDeployment,
+//   kepler: keplerDeployment,
+//   mainnet: mainnetDeployment,
+// };
 
 export enum ChainID {
   testnet = '0x13881',
-  kepler = '0x89',
+  local = '0x7A69',
   mainnet = '0x89',
 }
 
-export const ChainIDs = [ChainID.testnet, ChainID.kepler, ChainID.mainnet];
-
-type SubqueryNetwork = 'mainnet' | 'kepler' | 'testnet';
+export const ChainIDs = [ChainID.testnet, ChainID.local, ChainID.mainnet];
 
 function createContractOptions(network: SubqueryNetwork): SdkOptions {
   return {
-    // @ts-ignore
-    deploymentDetails: deployments[network],
     network,
+    // deploymentDetails: deployments[network],
   };
 }
 
 export const networkToChainID: Record<SubqueryNetwork, ChainID> = {
   testnet: ChainID.testnet,
-  kepler: ChainID.kepler,
+  local: ChainID.local,
   mainnet: ChainID.mainnet,
 };
 
 export const sdkOptions = {
   [ChainID.testnet]: createContractOptions('testnet'),
-  [ChainID.kepler]: createContractOptions('kepler'),
+  [ChainID.local]: createContractOptions('local'),
+  [ChainID.mainnet]: createContractOptions('mainnet'),
 };
 
 export function initProvider(endpoint: string, chainID: string) {
@@ -48,7 +46,7 @@ export function initProvider(endpoint: string, chainID: string) {
 
 export function initContractSDK(
   provider: providers.StaticJsonRpcProvider | Signer,
-  chainID: ChainID,
+  chainID: ChainID
 ): ContractSDK {
-  return new ContractSDK(provider, sdkOptions[chainID]);
+  return ContractSDK.create(provider, sdkOptions[chainID]);
 }
