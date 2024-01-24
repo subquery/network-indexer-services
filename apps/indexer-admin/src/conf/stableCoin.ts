1a2260db877d854b04b4ad0496e5e82f48dfe591
cyrbuzz <cyrbuzz@foxmail.com>
2024-01-24 14:50:10 +0800

    fix: usdc config

--- apps/indexer-admin/src/conf/stableCoin.ts
+++ apps/indexer-admin/src/conf/stableCoin.ts
@@ -1,9 +1,8 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { STABLE_COIN_ADDRESSES } from '@subql/network-config';
+
 import { SUPPORTED_NETWORK } from './rainbowConf';
 
-export const STABLE_COIN_ADDRESS =
-  SUPPORTED_NETWORK === 'testnet'
-    ? '0x3148eb6a6689a5a8a64b50ea8f845231d4617f9b'
-    : '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913';
+export const STABLE_COIN_ADDRESS = STABLE_COIN_ADDRESSES[SUPPORTED_NETWORK];
