6aecf237222eb811b310a47b870af01f25288123
Jacob <princewith@gmail.com>
2024-02-06 09:47:47 +0800

    implement auto collect allocation reward threshold

--- apps/indexer-coordinator/src/core/reward.service.ts
+++ apps/indexer-coordinator/src/core/reward.service.ts
@@ -3,12 +3,20 @@
 
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { BigNumber } from 'ethers';
 import { NetworkService } from 'src/network/network.service';
+import { getLogger } from 'src/utils/logger';
 import { AccountService } from './account.service';
 import { OnChainService } from './onchain.service';
 
 @Injectable()
 export class RewardService implements OnModuleInit {
+  private readonly rewardThreshold = BigNumber.from(1000 * 10 ** 18);
+  private readonly allocationBypassTimesLimit = 3;
+  private readonly allocationBypassTimes: Map<string, number> = new Map();
+
+  private readonly logger = getLogger('RewardService');
+
   constructor(
     private accountService: AccountService,
     private networkService: NetworkService,
@@ -17,11 +25,11 @@ export class RewardService implements OnModuleInit {
 
   onModuleInit() {
     // FIXME test only
-    this.autoClaimAllocationRewards();
+    this.autoCollectAllocationRewards();
   }
 
   @Cron('0 0 1 * * *')
-  async autoClaimAllocationRewards() {
+  async autoCollectAllocationRewards() {
     const indexerId = await this.accountService.getIndexer();
     const deploymentAllocations = await this.networkService.getIndexerAllocationSummaries(
       indexerId
@@ -34,7 +42,21 @@ export class RewardService implements OnModuleInit {
       if (rewards.eq(0)) {
         continue;
       }
+      const threshold = this.rewardThreshold;
+      const limit = this.allocationBypassTimesLimit;
+      const bypassTimes = this.allocationBypassTimes.get(allocation.deploymentId) || 0;
+      if (rewards.lt(threshold) && bypassTimes < limit) {
+        this.allocationBypassTimes.set(allocation.deploymentId, bypassTimes + 1);
+        this.logger.debug(
+          `Bypassed reward for deployment ${allocation.deploymentId} ${bypassTimes + 1}/${limit}`
+        );
+        continue;
+      }
       await this.onChainService.collectAllocationReward(allocation.deploymentId, indexerId);
+      this.allocationBypassTimes.delete(allocation.deploymentId);
+      this.logger.debug(
+        `Collected reward for deployment ${allocation.deploymentId} ${rewards.toString()}`
+      );
     }
   }
 }
