0c214f912d0480e922e84d45ec02caeee90b53ef
mzxyz <mzxyz@gmail.com>
2024-02-09 11:15:59 +1300

    Update copyright years in license header to 2020-2024

--- apps/indexer-admin/src/pages/project-details/types.ts
+++ apps/indexer-admin/src/pages/project-details/types.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
+// Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import { AsyncData } from '@subql/react-hooks';
