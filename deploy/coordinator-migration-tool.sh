806ddc8ef379a8d2314ab366a8df5201328b567f
Jacob <princewith@gmail.com>
2024-02-20 14:19:16 +0800

    Update coordinator and proxy versions

--- deploy/coordinator-migration-tool.sh
+++ deploy/coordinator-migration-tool.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 docker_compose_file="docker-compose.yml"
+coordinator_version_default="v2.0.0-58"
+proxy_version_default="v2.0.0-beta.16"
 
 prompt_question() {
   prompt=$1
   default_value=$2
 
-  echo -e $prompt >&2
-  read user_input
+  read -p "$prompt" user_input
 
   if [ -z "$user_input" ]; then
     echo $default_value
@@ -20,7 +21,7 @@ ask_for_confirmation() {
   prompt=$1
   default_value=$2
 
-  answer=$(prompt_question "$prompt (y/n, default is $default_value)" "$default_value")
+  answer=$(prompt_question "$prompt (y/n, default is $default_value): " "$default_value")
 
   if [ "$answer" != "y" ]; then
     echo "Operation cancelled."
@@ -34,7 +35,7 @@ ask_for_input() {
   prompt=$1
   default_value=$2
 
-  answer=$(prompt_question "$prompt (default is $default_value)" "$default_value")
+  answer=$(prompt_question "$prompt (default is $default_value): " "$default_value")
 
   echo $answer
 }
@@ -59,7 +60,7 @@ create_backup() {
 
   if file_exists "$origin_filename"; then
     if file_exists "$backup_filename"; then
-      answer=$(prompt_question "Backup file $backup_filename already exists.\nDo you want to overwrite it? (y/n, default is n)" "n")
+      answer=$(prompt_question "Backup file $backup_filename already exists. Do you want to overwrite it? (y/n, default is n): " "n")
       if [ "$answer" != "y" ]; then
         echo "Backup creation cancelled."
         new_line
@@ -102,7 +103,13 @@ replace_text() {
   text_to_search=$2
   text_to_replace=$3
 
-  sed -i "" "s/$text_to_search/$text_to_replace/g" "$file"
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    sed -i "" "s/$text_to_search/$text_to_replace/g" "$file"
+  else
+    # Linux
+    sed -i "s/$text_to_search/$text_to_replace/g" "$file"
+  fi
 }
 
 escape_url() {
@@ -118,14 +125,31 @@ modify_coordinator_ws_endpoint() {
 
 modify_network_type() {
   echo "Modifying network type..."
-  replace_text $docker_compose_file "--network-type=kepler" "--network-type=mainnet"
+  replace_text $docker_compose_file "--network=[^[:space:]]*" "--network=mainnet"
   echo "Network type updated."
+  new_line
+}
+
+modify_coordinator_version() {
+  echo "Modifying coordinator version..."
+  coordinator_version=$(ask_for_input "Enter the coordinator version (Leave empty to use default value)" "$coordinator_version_default")
+  replace_text $docker_compose_file "image: subquerynetwork\/indexer-coordinator:[^[:space:]]*" "image: subquerynetwork\/indexer-coordinator:$coordinator_version"
+  replace_text $docker_compose_file "image: subquerynetwork\/indexer-coordinator-dev:[^[:space:]]*" "image: subquerynetwork\/indexer-coordinator-dev:$coordinator_version"
+  echo "Coordinator version updated."
+  new_line
+}
+
+modify_proxy_version() {
+  echo "Modifying proxy version..."
+  proxy_version=$(ask_for_input "Enter the proxy version (Leave empty to use default value)" "$proxy_version_default")
+  replace_text $docker_compose_file "image: subquerynetwork\/indexer-proxy:[^[:space:]]*" "image: subquerynetwork\/indexer-proxy:$proxy_version"
+  echo "Proxy version updated."
   new_line
 }
 
 modify_network_endpoint() {
   echo "Modifying network endpoint..."
-  network_endpoint=$(ask_for_input "Enter the network endpoint below: (Leave empty to use default value)" "https://mainnet.base.org")
+  network_endpoint=$(ask_for_input "Enter the network endpoint (Leave empty to use default value)" "https://mainnet.base.org")
   network_endpoint=$(escape_url $network_endpoint)
   replace_text $docker_compose_file "--network-endpoint=[^[:space:]]*" "--network-endpoint=$network_endpoint"
   echo "Network endpoint updated."
@@ -134,7 +158,7 @@ modify_network_endpoint() {
 
 modify_database_name() {
   echo "Modifying database name..."
-  database_name=$(ask_for_input "Enter the database name below: (It SHOULD be different with your current one. If you never changed the previous default db name, you could leave it empty)" "coordinator_db")
+  database_name=$(ask_for_input "Enter the database name (It SHOULD be different with your current one. If you never changed the previous default db name, you could leave it empty)" "coordinator_db")
   if $(check_text_exists $docker_compose_file "postgres-database"); then
     replace_text $docker_compose_file "postgres-database=[^[:space:]]*" "postgres-database=$database_name"
   else
@@ -150,6 +174,8 @@ assert_file_exists $docker_compose_file
 create_backup $docker_compose_file
 modify_coordinator_ws_endpoint
 modify_network_type
+modify_coordinator_version
+modify_proxy_version
 modify_network_endpoint
 modify_database_name
 
