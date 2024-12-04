fcadc4fd96468dbe950ea1ac57ac38ba1f90ae0a
gerald <3949379+getong@users.noreply.github.com>
2024-12-04 14:34:49 +0800

    update code

--- apps/indexer-proxy/proxy/src/mod_libp2p/mod.rs
+++ apps/indexer-proxy/proxy/src/mod_libp2p/mod.rs
@@ -43,6 +43,8 @@ const TESTNET_ADDRESS: [&str; 2] = ["/ip4/192.168.1.136/tcp/8002", "/ip4/192.168
 
 pub async fn start_swarm() -> Result<(Swarm<AgentBehavior>, Keypair), Box<dyn Error + Send + Sync>>
 {
+    std::env::set_var("PRIVITE_NET_KEY", "wiwlLGQ8g6zu0mcckkROzeeAU7xN+Adz40ELWSH3f1M=");
+    std::env::set_var("PRIVITE_NET_ADDRESS", "/ip4/0.0.0.0/tcp/8004");
     let psk = get_psk();
     println!("file: {}, line: {}", file!(), line!());
     if let Ok(psk) = psk {
@@ -85,16 +87,16 @@ pub async fn start_swarm() -> Result<(Swarm<AgentBehavior>, Keypair), Box<dyn Er
             let kad_memory = KadInMemory::new(local_peer_id);
             let mut kad = KadBehavior::with_config(local_peer_id, kad_memory, kad_config);
 
-            for to_dial in TESTNET_ADDRESS {
-                if let Ok(addr) = parse_legacy_multiaddr(&to_dial) {
-                    for peer_address in &BOOTNODES {
-                        if let Ok(peer) = PeerId::from_str(peer_address) {
-                            kad.add_address(&peer, addr.clone());
-                        }
-                    }
-                }
-            }
-            _ = kad.bootstrap();
+            // for to_dial in TESTNET_ADDRESS {
+            //     if let Ok(addr) = parse_legacy_multiaddr(&to_dial) {
+            //         for peer_address in &BOOTNODES {
+            //             if let Ok(peer) = PeerId::from_str(peer_address) {
+            //                 kad.add_address(&peer, addr.clone());
+            //             }
+            //         }
+            //     }
+            // }
+            // _ = kad.bootstrap();
 
             let identify_config =
                 IdentifyConfig::new("/agent/connection/1.0.0".to_string(), key.clone().public())
@@ -160,6 +162,23 @@ pub async fn start_swarm() -> Result<(Swarm<AgentBehavior>, Keypair), Box<dyn Er
 pub async fn handle_swarm_event(mut swarm: Swarm<AgentBehavior>, local_key: Keypair) {
     let mut interval1 = time::interval(Duration::from_secs(8));
     let mut interval2 = time::interval(Duration::from_secs(16));
+    let local_peer_id = local_key.public().to_peer_id();
+    let request = GreeRequest {
+        message: format!("Send message from: {local_peer_id}: Hello gaess"),
+    };
+    let resquest_message = AgentMessage::GreeRequest(request);
+    for peer_id_address in BOOTNODES {
+        match peer_id_address.parse() {
+            Ok(peer_id ) => {
+                let request_id = swarm
+                    .behaviour_mut()
+                    .send_message(&peer_id, resquest_message.clone());
+                warn!("peer_id_address: {peer_id_address}, peer_id: {peer_id:?}, RequestID: {request_id}, resquest_message : {resquest_message:?}")
+            },
+            Err(err) => warn!("err is {:?}, peer_id_address is {}", err, peer_id_address),
+        }
+
+    }
     tokio::spawn(async move {
         loop {
             tokio::select! {
